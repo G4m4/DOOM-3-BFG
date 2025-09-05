395c36e11878f8d5f3c30cb1dbff5b7d7c0f7509
g4m4 <misept.dieseneuf@gmail.com>
2025-09-05 17:45:39 +0200

    [RENDERER] Fix pointer parameter constness

--- neo/renderer/RenderSystem_init.cpp
+++ neo/renderer/RenderSystem_init.cpp
@@ -341,7 +341,7 @@ For ARB_debug_output
 ========================
 */
 static void DebugCallback(GLenum source, GLenum type,
-						  GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam) {
+						  GLuint id, GLenum severity, GLsizei length, const GLchar* message, const GLvoid* userParam) {
 #if defined(ID_PC_WIN)
 	// it probably isn't safe to do an idLib::Printf at this point
 	OutputDebugString( message );
