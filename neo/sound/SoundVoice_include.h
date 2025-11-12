bc64a215a2f84e289bcef3c774467a9a752f176f
g4m4 <misept.dieseneuf@gmail.com>
2025-11-12 15:34:22 +0100

    [NEO] Fix bad include path

--- neo/sound/SoundVoice_include.h
+++ neo/sound/SoundVoice_include.h
@@ -29,7 +29,7 @@ If you have questions concerning this license or the applicable additional terms
 #define __SOUNDVOICE_INCLUDE_H__
 
 #if defined(ID_PC_WIN)
-#include "sound/XAudio/XA2_SoundVoice.h"
+#include "sound/XAudio2/XA2_SoundVoice.h"
 #elif defined(ID_PC_LINUX)
 #include "sound/FAudio/FAudio_SoundVoice.h"
 #else
